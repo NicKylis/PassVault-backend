6f35ea434ac48ffd8634d2c6023624690c536d55
ChTor <xristina.1xr@gmail.com>
2025-11-15 13:21:32 +0200

    First commit

---
+++ models/Password.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const PasswordSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      default: null,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    website: {
+      type: String,
+      default: null,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    category: {
+      type: String,
+      enum: ["Social Media", "Email", "ECommerce", "Banking", "Other"],
+      default: "Other",
+    },
+    passwordStrength: {
+      type: String,
+      enum: ["Weak", "Good", "Strong"],
+      default: "Good",
+    },
+    notes: {
+      type: String,
+      default: null,
+    },
+    lastUsedAt: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+PasswordSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: (_, ret) => {
+    ret.id = ret._id;
+    delete ret._id;
+  },
+});
+
+const Password = mongoose.model("Password", PasswordSchema);
+
+export default Password;
